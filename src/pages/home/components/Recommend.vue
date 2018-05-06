527432c14ea8f245a0efa5eee68f7cbda4070eab
jinyu73 <1115741577@qq.com>
2018-05-06 21:32:10 +0800

    add recommend

---
+++ src/pages/home/components/Recommend.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+    <div class="title">热销推荐</div>
+    <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+            <img class="item-img" :src="item.imgUrl"/>
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+                <button class="item-button">查看详情</button>
+            </div>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeRecommend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1804/3c/3c1d865ea3175504a3.img.jpg_200x200_8844d692.jpg',
+        title: '千岛湖',
+        desc: '千年迷魂镇，据传是三国孙权的故里'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1804/3c/3c1d865ea3175504a3.img.jpg_200x200_8844d692.jpg',
+        title: '千岛湖',
+        desc: '千年迷魂镇，据传是三国孙权的故里'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1804/3c/3c1d865ea3175504a3.img.jpg_200x200_8844d692.jpg',
+        title: '千岛湖',
+        desc: '千年迷魂镇，据传是三国孙权的故里'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/p0/1804/3c/3c1d865ea3175504a3.img.jpg_200x200_8844d692.jpg',
+        title: '千岛湖',
+        desc: '千年迷魂镇，据传是三国孙权的故里'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/mixins.styl'
+    .title
+        margin-top: .2rem
+        line-height: .8rem
+        background:#eee
+        text-indent:.2rem
+    .item
+        overflow: hidden
+        display: flex
+        height: 1.9rem
+        .item-img
+            width:1.7rem
+            height:1.7rem
+            padding: .1rem
+        .item-info
+            flex: 1
+            padding: .1rem
+            min-width: 0
+            .item-title
+                line-height:.54rem
+                font-size: .32rem
+                ellipsis()
+            .item-desc
+                line-height: .4rem
+                color:#ccc
+                ellipsis()
+            .item-button
+                line-height: .44rem
+                margin-top: .16rem
+                background:#ff9300
+                padding: 0 .2rem
+                border-radius: .06rem
+                color: #fff
+</style>
---
+++ src/pages/home/components/Weekend.vue
@@ -0,0 +1,74 @@
+<template>
+  <div>
+    <div class="title">周末去哪玩</div>
+    <ul>
+        <li class="item border-bottom" v-for="item of recommendList" :key="item.id">
+            <div class="item-img-wrapper">
+                <img class="item-img" :src="item.imgUrl"/>
+            </div>
+            <div class="item-info">
+                <p class="item-title">{{item.title}}</p>
+                <p class="item-desc">{{item.desc}}</p>
+            </div>
+        </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeWeekend',
+  data () {
+    return {
+      recommendList: [{
+        id: '0001',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4e/3e87ee62e59cdb.jpg_r_640x214_fe6cc902.jpg',
+        title: '千岛湖',
+        desc: '千年迷魂镇，据传是三国孙权的故里'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4e/3e87ee62e59cdb.jpg_r_640x214_fe6cc902.jpg',
+        title: '千岛湖',
+        desc: '千年迷魂镇，据传是三国孙权的故里'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4e/3e87ee62e59cdb.jpg_r_640x214_fe6cc902.jpg',
+        title: '千岛湖',
+        desc: '千年迷魂镇，据传是三国孙权的故里'
+      }, {
+        id: '0004',
+        imgUrl: 'http://img1.qunarzz.com/sight/source/1505/4e/3e87ee62e59cdb.jpg_r_640x214_fe6cc902.jpg',
+        title: '千岛湖',
+        desc: '千年迷魂镇，据传是三国孙权的故里'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+    @import '~styles/mixins.styl'
+    .title
+        margin-top: .2rem
+        line-height: .8rem
+        background:#eee
+        text-indent:.2rem
+    .item-img-wrapper
+        overflow: hidden
+        width: 100%
+        height: 0
+        padding-bottom: 37.09%
+        background:red
+        .item-img
+            width:100%
+    .item-info
+        padding: .1rem
+        .item-title
+            line-height:.54rem
+            font-size: .32rem
+            ellipsis()
+        .item-desc
+            line-height: .4rem
+            color:#ccc
+            ellipsis()
+</style>
