368519f5a806f099754db80753732f1c198bd4f0
jinyu73 <1115741577@qq.com>
2018-05-08 22:31:00 +0800

    peerfomance

--- src/pages/city/components/List.vue
+++ src/pages/city/components/List.vue
@@ -20,7 +20,12 @@
                     </div>
                 </div>
             </div>
-            <div class="area" v-for="(item, key) of cities" :key="key">
+            <div
+                class="area"
+                v-for="(item, key) of cities"
+                :key="key"
+                :ref="key"
+                >
                 <div class="title  border-topbottom">{{key}}</div>
                 <div class="item-list">
                     <div
@@ -40,10 +45,20 @@ export default {
   name: 'CityList',
   props: {
     hot: Array,
-    cities: Object
+    cities: Object,
+    letter: String
   },
   mounted () {
     this.scroll = new BScroll(this.$refs.wrapper)
+  },
+  watch: {
+    letter: function () {
+      if (this.letter) {
+        const element = this.$refs[this.letter][0]
+        console.log(element)
+        this.scroll.scrollToElement(element)
+      }
+    }
   }
 }
 </script>
